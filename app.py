332948309df2785ffe34fd2fa3387cba102bca64
MicahJank <MicahJank@users.noreply.github.com>
2020-10-05 12:11:50 -0500

    got most of the html scraped off the just need to work on the one post that has a lot of different html elements inside it, that one is not working

--- app.py
+++ app.py
@@ -1,26 +1,42 @@
 from flask import Flask, render_template, url_for, redirect
 import requests
+import pprint
+# lxml can help me scrape the data from the html
+from lxml import html
 
 app = Flask(__name__)
 
-url = "https://uifaces.co/api?limit=5"
+url = "https://api.dailysmarty.com/posts"
 
 # querystring = {"location":"de","registrar":"dnsimple.com","defaults":"club%2Ccoffee","query":"acme cafe"}
 
-headers = {
-    'X-API-KEY': "10B30A17-B65D468A-913ABEED-7E36BEB8",
-    'Accept': 'application/json',
-	'Cache-Control': 'no-cache'
-    }
+# headers = {
+#     'X-API-KEY': "10B30A17-B65D468A-913ABEED-7E36BEB8",
+#     'Accept': 'application/json',
+# 	'Cache-Control': 'no-cache'
+#     }
 
 
 @app.route('/')
 def index():
     response = requests.get(url)
 
-    domains = response.json()
-    print(domains)
-    return render_template('index.html', domains=domains)
+    posts = response.json()["posts"]
+    content = ''
+
+    for post in posts:
+        tree = html.fromstring(post['content'])
+        content = tree.xpath('//p/text()')
+        if len(content) > 0:
+            post['content'] = content[0]
+
+        print(type(content))
+        print(pprint.pprint(post))
+        
+    # print(pprint.pprint(posts))
+    # tree = html.fromstring(posts["posts"]["content"])
+    # print(tree)
+    return render_template('index.html', posts=posts)
 
 
 
